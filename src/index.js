f9eadfe8e1a20d67ec7d71a76b267781b805f706
TatianaFF <tsff2018@gmail.com>
2024-02-01 17:19:39 +0700

    BrouserRouter -> HashRouter

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import store from "./redux/reduxStore";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={ store }>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
 
